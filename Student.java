d1405b0658dfa289dcaa52a58dd3cef270b45116
li <ktonneh@thecodevillage.com>
2020-07-02 19:13:11 +0300

    thecodevillage

---
+++ Student.java
@@ -0,0 +1,15 @@
+
+public class Student {
+
+public static void main(String[] args){
+
+  int age = 20;
+  String name = "John Doe";
+  String studentClass = "Form 1";
+
+  System.out.println("Name: "+name);
+  System.out.println("Age: "+age);
+  System.out.println("Class: "+studentClass);
+}
+
+}
