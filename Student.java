cb814032aea8e847dffce85e3c6703579e575b95
li <ktonneh@thecodevillage.com>
2020-07-02 19:31:15 +0300

    thecodevillage

--- Student.java
+++ Student.java
@@ -6,10 +6,15 @@ public static void main(String[] args){
   int age = 20;
   String name = "John Doe";
   String studentClass = "Form 1";
+  String address = "Nairobi";
+  String admissionNumber = "GHD23242";
 
   System.out.println("Name: "+name);
   System.out.println("Age: "+age);
   System.out.println("Class: "+studentClass);
+
+  System.out.println("Address: "+address);
+  System.out.println("Admission Number: "+admissionNumber);
 }
 
 }
